f05fc776affd6dfbb4ce2dbd8ed9e72e813e9f77
am0392 <andymorgan@hotmail.nl>
2026-06-24 21:05:43 +0200

    Newsfeed toegevoegd

--- boekenclub/base/views.py
+++ boekenclub/base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import ReadingSession
 
 # Create your views here.
 def index(request):
     return render(request, "base/index.html")
+
+def newsfeed(request):
+    sessions = ReadingSession.objects.all()
+    context = {"sessions": sessions}
+    return render(request, "base/newsfeed.html", context)
