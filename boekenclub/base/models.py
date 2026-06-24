aa558728c1bac2e474b402cbbe797206e38faef4
am0392 <andymorgan@hotmail.nl>
2026-06-24 20:04:31 +0200

    Models + Index page + Menu bar
    Wait it's all yoshi?
    
    Always has been

--- boekenclub/base/models.py
+++ boekenclub/base/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    FavoriteGenre = models.CharField(max_length=100)
+    DateOfBirth = models.DateField(null=True, blank=True)
+    City = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Book(models.Model):
+    Name = models.CharField(max_length=30, blank=False, unique=True)
+    PublicationYear = models.IntegerField(blank=False)
+    Genre = models.CharField(max_length=30, blank=True)
+    Approved = models.BooleanField()
+    ApprovedBy = models.ForeignKey(User, on_delete=models.CASCADE,
+                                    related_name='ApprovedBy', null=True, blank=True)
+    def __str__(self):
+        return self.Name
+
+
+class ReadingSession(models.Model):
+    Book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, null=False)
+    Date = models.DateField(blank=False,)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    Score = models.IntegerField()
+    def __str__(self):
+        return f"Book: {Book.Name}, Score: {self.Score}, Read by: {self.User.get_username()}"
