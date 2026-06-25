d0f97cebdff3462c2e7bcf64c4f3fa545b7450c8
irish280 <hanxuen8200@gmail.com>
2026-06-25 19:50:30 +0200

    gebruiker kan inloggen

--- boekenclub/base/urls.py
+++ boekenclub/base/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("", include('django.contrib.auth.urls')),
+    path("register/", views.register, name="register"),
+    
+    #path("nameform/", views.nameForm, name="nameform")
 ]
--- boekenclub/base/views.py
+++ boekenclub/base/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 
+#from .forms import ProfileForm
+from .forms import RegisterForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 # Create your views here.
 def index(request):
     return render(request, "base/index.html")
+
+'''
+def ProfileForm(requests):
+    form = ProfileForm()
+    context = {"form": form}
+
+    if requests.method == "POST":
+        name = requests.POST.get("your_name")
+        context["greeting"] = f"What would you do if when you okay, so he said yes would go,  {name}?"
+    return render(requests, "base/nameform.html", context)
+'''
+
+def register(request):
+    if request.method == "POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # log the user in and redirect to index
+            login(request, user)
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
