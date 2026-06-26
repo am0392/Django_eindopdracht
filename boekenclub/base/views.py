4d5a5b7645401a565cb340a94ec8b1b2c4544c30
thanya0092-bit <thanya0092@gmail.com>
2026-06-26 23:17:33 +0200

    CSS messagas + add delete button

--- boekenclub/base/views.py
+++ boekenclub/base/views.py
@@ -7,6 +7,7 @@ from .forms import RegisterForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from .forms import ProfileForm
+from django.db import IntegrityError
 
 
 def index(request):
@@ -22,11 +23,14 @@ def sessionform(request):
     if request.method == 'POST':
         form = ReadingSessionForm(request.POST)
         if form.is_valid():
-            session = form.save(commit=False)
-            session.User = request.user
-            session.save()
-            messages.success(request, 'Reading session added successfully.')
-            return redirect('RecentSessions')
+            try:
+                session = form.save(commit=False)
+                session.User = request.user
+                session.save()
+                messages.success(request, 'Reading session added successfully.')
+                return redirect('RecentSessions')
+            except IntegrityError:
+                messages.error(request, 'You already have a reading session for this book on this date.')
     else:
         form = ReadingSessionForm()
     return render(request, 'base/session.html', {'form': form})
@@ -36,13 +40,17 @@ def edit_session(request, pk):
     session = ReadingSession.objects.get(pk=pk)
     if request.method == 'POST':
         form = ReadingSessionForm(request.POST, instance=session)
+        if request.POST.get("Delete"):
+            session.delete()
+            messages.success(request, "Session deleted")
+            return redirect('RecentSessions')
         if form.is_valid():
             form.save()
             messages.success(request, 'Reading session updated successfully.')
             return redirect('RecentSessions')
     else:
         form = ReadingSessionForm(instance=session)
-    context = {'form': form, 'session': session}
+    context = {'form': form, 'session': session, "edit": True}
     return render(request, 'base/reading_session_form.html', context)
 
 def session_list(request):
