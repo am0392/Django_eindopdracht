73d02ea525d925c3f346a34badea5b7919f12de4
irish280 <hanxuen8200@gmail.com>
2026-06-28 21:43:35 +0200

    je kan wachtwoord veranderen

--- boekenclub/base/views.py
+++ boekenclub/base/views.py
@@ -11,6 +11,10 @@ from .forms import ProfileForm
 from django.db import IntegrityError
 from .models import Profile
 
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+
+
 
 def index(request):
     return render(request, "base/index.html")
@@ -37,6 +41,22 @@ def sessionform(request):
         form = ReadingSessionForm()
     return render(request, 'base/session.html', {'form': form})
 
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'base/change_password.html', {
+        'form': form
+    })
 
 def edit_session(request, pk):
     session = ReadingSession.objects.get(pk=pk)
